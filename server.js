a21c3fac6f332b19e197bf42375fc716f05da5f0
soumen-github-account <soumendas7584@gmail.com>
2026-04-30 20:16:40 +0530

    updated yaml

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ import express from "express"
 const app = express();
 const PORT = process.env.PORT ?? 8000
 
-// app.get('/', (req, res)=>{
-//     return res.json({msg: "Hello from the server"})
-// })
+app.get('/', (req, res)=>{
+    return res.json({msg: "Hello from the server"})
+})
 
-app.get("/", (req, res) => {
-  res.send(Hello); // ❌ undefined variable
-});
+// app.get("/", (req, res) => {
+//   res.send(Hello); // ❌ undefined variable
+// });
 
 app.listen(PORT, ()=>{
     console.log(`server is running on port : ${PORT}`);
